62b6cc1fae2f86363bd6df94c3b6ccd642a47284
Katie <kacarr@smu.edu>
2019-02-25 15:33:39 -0600

    Made changes to the user calendar component
    Also added functional navigation between tabs in SimpleAppBar component.

--- src/components/Course/CourseAnnouncements/CourseAnnouncements.jsx
+++ src/components/Course/CourseAnnouncements/CourseAnnouncements.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 import "./courseAnnouncements.scss";
-import SimpleAppBar from "../../UserSettings/SimpleAppBar/SimpleAppBar";
+import SimpleAppBar from "../../SimpleAppBar/SimpleAppBar";
 import Typography from "@material-ui/core/Typography/Typography";
 import Button from "@material-ui/core/Button/Button";
 import Avatar from "../../UserSettings/Avatar/Avatar";
--- src/components/SimpleAppBar/SimpleAppBar.jsx
+++ src/components/SimpleAppBar/SimpleAppBar.jsx
@@ -7,6 +7,27 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Grid from "@material-ui/core/Grid/Grid";
 import { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/CC Logo.png";
+
+const navTabs = [
+  {
+    label: "Settings",
+    path: "/UserSettings"
+  },
+  {
+    label: "Dashboard",
+    path: "/UserDashboard"
+  },
+  {
+    label: "Courses",
+    path: "/MyCourses"
+  },
+  {
+    label: "Calendar",
+    path: "/UserCalendar"
+  }
+];
 
 function SimpleAppBar(props) {
   const [val, setValue] = useState(props.value);
@@ -19,12 +40,7 @@ function SimpleAppBar(props) {
         <Grid container direction="row" xs={12} spacing={0}>
           <Grid item xs={6}>
             <Toolbar>
-              <img
-                src="../../../assets/CoachesCLASS.jpeg"
-                alt=""
-                height="25"
-                width="25"
-              />
+              <img src={logo} alt="" height="25" width="25" className="pr-4" />
 
               <Typography variant="h6" color="primary">
                 {props.title}
@@ -33,20 +49,24 @@ function SimpleAppBar(props) {
           </Grid>
           <Grid item xs={6}>
             <Tabs value={val} indicatorColor="primary" textColor="primary">
-              <Tab
-                onClick={() => setValue(0)}
-                label="User Settings"
-                // to="/UserSettings"
-                // component={Link}
-              />
-              <Tab
+              {navTabs.map(tab => (
+                <NavLink to={tab.path} className="navLink">
+                  <Tab
+                    onClick={() => setValue(0)}
+                    label={tab.label}
+                    // to="/UserSettings"
+                    // component={Link}
+                  />
+                </NavLink>
+              ))}
+              {/* <Tab
                 onClick={() => setValue(1)}
                 label="Dashboard"
                 // to="/UserDashboard"
                 // component={Link}
               />
               <Tab onClick={() => setValue(2)} label="My Courses" />
-              <Tab onClick={() => setValue(3)} label="Calendar" />
+              <Tab onClick={() => setValue(3)} label="Calendar" /> */}
             </Tabs>
           </Grid>
         </Grid>
